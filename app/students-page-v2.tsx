b4acc91f214f8d0ef4ddb7d7b32a66e00764184a
hudbrother62-coder <hudbrother62@gmail.com>
2026-09-21 11:45:14 +0700

    style: scope compact student page layout

--- app/students-page-v2.tsx
+++ app/students-page-v2.tsx
@@ -4,7 +4,7 @@ import {StudentArchiveTools} from './student-archive-tools';
 
 export default function StudentsPageV2(props:any){
  const activeStudents=(props.students||[]).filter((s:any)=>s.status!=='inactive');
- return <div>
+ return <div className="student-page-v2">
   <StudentArchiveTools school={props.school} students={props.students||[]} reload={props.reload}/>
   <OfficialStudentsPage school={props.school} students={activeStudents} reload={props.reload}/>
  </div>;
